d284aa3032252c9f8556460cd08015c3e8f3b88a
Squabbi <squabbi@users.noreply.github.com>
2018-12-19 17:03:54 +1100

    Added make files for gt5note8ltexx.

---
+++ omni_gt5note8ltexx.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2016 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# TODO: Add this file.
+#$(call inherit-product, device/motorola/harpia/full_harpia.mk)
+
+PRODUCT_NAME := omni_gt5note8ltexx
