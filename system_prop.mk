41473ad7f9bd9980edb3c610c8a36888e4aab74a
Squabbi <squabbi@users.noreply.github.com>
2018-12-19 17:45:43 +1100

    Created system make file.

---
+++ system_prop.mk
@@ -0,0 +1,11 @@
+# CABL
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.qualcomm.cabl=2
+
+# Screen
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=160
+
+# Telephony
+PRODUCT_PROPERTY_OVERRIDES += \
+	telephony.lteOnCdmaDevice=0
